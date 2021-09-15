3f91c4b33eb0840b9036d764904404929753b3d8
wizz <amar.malkic96@gmail.com>
2021-09-15 17:14:54 +0200

    index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -2,7 +2,7 @@ function main() {
     //Only change code below this line 
     //5 letters
     /* jos 5 letters */
-    //Only change code below this line 
+    //Only change code above this line 
 }
 
 console.log(main());
