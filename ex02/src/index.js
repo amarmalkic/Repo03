787e596921b3b2dc94787adfd0416c3739a35d5f
wizz <amar.malkic96@gmail.com>
2021-09-15 17:10:28 +0200

    comm

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    //Only change code below this line 
+    //5 letters
+    /* jos 5 letters */
+    //Only change code below this line 
+}
+
+console.log(main());
+
+module.exports = main;
