798463a1b43667160dd6b4df17e6800abcde77ae
wizz <amar.malkic96@gmail.com>
2021-09-15 17:25:04 +0200

    var

---
+++ ex03/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    // Only change code below this line
+    var forCat = semicolon;
+    forCat = semicolon;
+    // Only change code above this line
+}
+console.log(main());
+module.exports = main;
